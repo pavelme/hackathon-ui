98bf386420535318aad6e615756f1e3a3cdf4d7a
elenabo <elenabo@wix.com>
2015-10-15 19:37:02 +0300

    add product view

--- app/controllers/ProductsController.scala
+++ app/controllers/ProductsController.scala
@@ -1,11 +1,35 @@
 package controllers
 
-import play.api.mvc._
+import play.api.Play.current
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n.Messages.Implicits._
+import play.api.mvc.{Action, Controller}
 
 object ProductsController extends Controller {
 
   def products = Action {
     Ok(views.html.Products.products())
+  }
+
+  val productUrlForm = Form(
+    "Product url" -> nonEmptyText
+  )
+
+
+  def getProducts =
+    Action { implicit request =>
+      productUrlForm.bindFromRequest.fold(
+        errors => BadRequest(views.html.addProduct2(errors.errorsAsJson.toString)),
+        url => {
+          Ok(url)
+        }
+      )
+    }
+
+
+  def sendProductUrl = Action {
+    Ok(views.html.addProduct(productUrlForm))
   }
 
 }
